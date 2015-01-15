1ea6d2a2f15dacf0448d50c87cab16cad956746a
Knuk <samuellapointe@gmail.com>
2015-01-15 15:15:43 -0500

    Create Project

---
+++ MinecraftBot/MinecraftBot/MinecraftBot.cpp
@@ -0,0 +1,18 @@
+// MinecraftBot.cpp : main project file.
+
+#include "stdafx.h"
+#include "Form1.h"
+
+using namespace MinecraftBot;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	// Enabling Windows XP visual effects before any controls are created
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false); 
+
+	// Create the main window and run it
+	Application::Run(gcnew Form1());
+	return 0;
+}
