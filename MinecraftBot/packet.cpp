0471061a0c393808819f26e92df99fc9485815e0
Knuk <samuellapointe@gmail.com>
2015-01-27 14:43:33 -0500

    Packets might work

--- MinecraftBot/packet.cpp
+++ MinecraftBot/packet.cpp
@@ -3,9 +3,10 @@
  * */
 #include "packet.h"
 
-Packet::Packet()
+Packet::Packet(const uint32_t id, const vector<char> &d)
 {
-
+    packetID = id;
+    data = d;
 }
 
 Packet::~Packet()
@@ -13,3 +14,35 @@ Packet::~Packet()
 
 }
 
+/* Packet format:
+ * First, the size of the packet as a varint
+ * then, the ID of the packet as a varint
+ * finally, the data itself. */
+vector<char> Packet::packPacket()
+{
+    uint8_t length = 4 + data.size(); //size of ID is 4
+    uint32_t varintLength;
+    Varint::encode_signed_varint(&length, varintLength);
+
+    vector<char> tmp;
+    tmp.push_back(varintLength);
+    tmp.push_back(length);
+    tmp.insert(tmp.end(), data.begin(), data.end());
+    return tmp;
+}
+
+/* String format:
+ * First, the length in bytes of the string as a varint
+ * Then, the string itself */
+vector<char> Packet::packString(const string text)
+{
+    uint8_t length = text.length(); //size of ID is 4
+    uint32_t varintLength;
+    Varint::encode_signed_varint(&length, varintLength);
+
+    vector<char> tmp;
+    tmp.push_back(varintLength);
+    tmp.insert(tmp.end(),text.begin(),text.end());
+
+    return tmp;
+}
--- MinecraftBot/packet.h
+++ MinecraftBot/packet.h
@@ -4,12 +4,23 @@
 #ifndef PACKET_H
 #define PACKET_H
 
+#include "varint.h"
+#include <vector>
+#include <string>
+
+using namespace std;
 
 class Packet
 {
 public:
-    Packet();
+    Packet(const uint32_t id, const vector<char> &data);
     ~Packet();
+    vector<char> packPacket();
+    vector<char> packString(const string &text);
+private:
+    uint32_t length;
+    uint32_t packetID;
+    vector<char> data;
 };
 
 #endif // PACKET_H
