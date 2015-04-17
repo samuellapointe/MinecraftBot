091575c7baae9e5612341bef987329fc59b217d8
Knuk <samuellapointe@gmail.com>
2015-04-17 09:31:21 -0400

    Chat support for spigot

--- MinecraftBot/chatmessage.cpp
+++ MinecraftBot/chatmessage.cpp
@@ -16,8 +16,35 @@ ChatMessage::ChatMessage(MyTcpSocket * s, MainWindow * i_ui, QByteArray &d, Comm
 
     QJsonDocument messageDoc = QJsonDocument::fromJson(message);
     QJsonObject messageObj = messageDoc.object();
-    QString translate = messageObj.value("translate").toString();
+    QJsonValue translateValue = messageObj.value("translate");
+    QString translate = translateValue.toString();
 
+    if(translateValue.isUndefined()) //Assuming the server is running spigot
+    {
+        QJsonArray extra = messageObj.value("extra").toArray();
+        if(extra.size() >= 4)
+        {
+            QJsonObject nameContainer = extra.at(2).toObject();
+            QJsonArray extraName = nameContainer.value("extra").toArray();
+            nameContainer = extraName.at(0).toObject();
+            QString username = nameContainer.value("text").toString();
+
+            QJsonObject messageContainer = extra.at(4).toObject();
+            QJsonArray extraMessage = messageContainer.value("extra").toArray();
+            messageContainer = extraMessage.at(0).toObject();
+            QString text = messageContainer.value("text").toString();
+            //Spigot adds ": ";
+            if(text.length() > 2)
+            {
+                text.remove(0, 2);
+                ui->writeToChat(username + ": " + text);
+                if(text.at(0) == '!')
+                {
+                    commandManager->readCommand(text, username);
+                }
+            }
+        }
+    }
     if(translate.compare("chat.type.text") == 0) //Regular chat message
     {
         QJsonArray messageContent = messageObj.value("with").toArray();
